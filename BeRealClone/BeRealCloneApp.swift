94edb842480342dae49cd8918d4a84628ce1e07f
Victoria Mckinnie <Vmcki004@fiu.edu>
2024-11-14 22:42:54 -0500

    Initial Commit

---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by user on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
